442c3b08b090a476965ba1fa230e3db7d654ad56
mitresh lukhi <mitresh.22397@gmail.com>
2022-12-03 11:47:20 +0000

    manual testing topics

---
+++ testing.java
@@ -0,0 +1,12 @@
+package week1;
+
+public class testing {
+    public static void main(String[] argd) {
+        System.out.println("WHAT IS SOFTWARE TESTING?");
+        System.out.println("What are the benefits of software testing?");
+        System.out.println("Qualification of tester");
+        System.out.println("Challanges of tester");
+        System.out.println("SDLC and STLC cycles");
+
+    }
+}
